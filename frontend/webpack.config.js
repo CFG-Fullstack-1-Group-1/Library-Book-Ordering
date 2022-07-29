0ce296738649baac6f879329514015fd6e67988b
Marga <margabovertorres@gmail.com>
2022-07-29 18:10:46 +0100

    Fix last problems and rm the dev server

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -3,15 +3,8 @@ const webpack = require("webpack");
 
 const DEV_SERVER_PORT = 8080;
 
-// Debugged using {https://stackoverflow.com/questions/66772358/webpack-warning-warning-in-defineplugin-conflicting-values-for-process-env-no}
-// and https://stackoverflow.com/questions/55652077/css-file-causing-error-in-react-webpack-configuration
-
 module.exports = {
   entry: path.resolve(__dirname, "./src/index.js"),
-  devServer: {
-    static: path.join(__dirname, "dist"),
-    port: DEV_SERVER_PORT,
-  },
   output: {
     path: path.resolve(__dirname, "./static/frontend"),
     filename: "[name].js",
@@ -26,21 +19,9 @@ module.exports = {
         use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(png|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.svg$/,
-        use: ["@svgr/webpack", "url-loader"],
-      },
-      {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ["babel-loader"],
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
