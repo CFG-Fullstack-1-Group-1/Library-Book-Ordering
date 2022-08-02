2acc0d7688fe3cc6bcae5985b9a852a675f92320
Eleanor CM <97481864+EleanorCM@users.noreply.github.com>
2022-08-02 15:36:30 +0100

    Cleaned up commented-out code, added package-lock.json to gitignore, removed migrations from gitignore, deleted temporary file

--- api/serializers.py
+++ api/serializers.py
@@ -14,33 +14,6 @@ class BookSerializer(serializers.ModelSerializer):
                   'category', 'isbn_10', 'isbn_13', 'google_books_id']
 
 
-
-# # ATTEMPT 1 
-# class OrderSerializer(serializers.ModelSerializer):
-#     book = BookSerializer()
-#     borrower = BorrowerSerializer()
-
-#     def create(self, request):
-#         # Populates Borrower table with borrower data provided in request
-#         borrower_data = request.pop('borrower')
-#         Borrower.objects.create(**borrower_data)
-
-#         # Populates Book table with book data provided in request
-#         book_data = request.pop('book')
-#         Book.objects.create(**book_data)
-        
-#         # Populates Order table with Order data (status, created, modified) from the request, then obtains and populates Foreign key fields book_id and borrower_id from respective tables
-#         order_data = Order.objects.create(**request)
-#         return order_data
-
-#     class Meta:
-#         model = Order
-#         fields = ['id', 'borrower',
-#                   'book', 'status', 'created', 'modified']
-
-
-
-# ATTEMPT 2 - The error message says that the .save() object I'm pointing to on line 57 is a Nonetype
 class OrderSerializer(serializers.ModelSerializer):
     book = BookSerializer()
     borrower = BorrowerSerializer()
@@ -56,7 +29,7 @@ class OrderSerializer(serializers.ModelSerializer):
         book_save = Book(**book_data)
         book_save.save(force_insert=True)
         
-        # Populates Order table with Order data (status, created, modified) from the request, then obtains and populates Foreign key fields book_id and borrower_id from respective tables
+        # Populates Order table with Order data (status, created, modified) and Foreign key fields book_id and borrower_id
         order_data = Order.objects.create(borrower = borrower_save, book = book_save, **request)        
         return order_data
 
@@ -64,30 +37,3 @@ class OrderSerializer(serializers.ModelSerializer):
         model = Order
         fields = ['id', 'borrower',
                   'book', 'status', 'created', 'modified']
-
-
-
-# # ATTEMPT 3 - Using Django .filter()
-# class OrderSerializer(serializers.ModelSerializer):
-#     book = BookSerializer()
-#     borrower = BorrowerSerializer()
-
-#     def create(self, request):
-#         # Populates Borrower table with borrower data provided in request
-#         borrower_data = request.pop('borrower')
-#         Borrower.objects.create(**borrower_data)
-#         borrower_id_filtered = Borrower.objects.filter(borrower=object.id).values_list('id', flat=True)
-
-#         # Populates Book table with book data provided in request
-#         book_data = request.pop('book')
-#         Book.objects.create(**book_data)
-#         book_id_filtered = Book.objects.filter(book=object.id).values_list('id', flat=True)
-        
-#         # Populates Order table with Order data (status, created, modified) from the request, then obtains and populates Foreign key fields book_id and borrower_id from respective tables
-         
-#         order_data = Order.objects.create(borrower = borrower_id_filtered, book = book_id_filtered, **request)        
-#         return order_data
-    # class Meta:
-    #     model = Order
-    #     fields = ['id', 'borrower',
-    #               'book', 'status', 'created', 'modified']
