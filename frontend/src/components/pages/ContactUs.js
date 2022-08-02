e9e9d618e3886a78f907d93f6e77409178197048
Marga <margabovertorres@gmail.com>
2022-08-02 18:02:35 +0100

    Added ContactUs.css + some modifications ContactUs.js

--- frontend/src/components/pages/ContactUs.js
+++ frontend/src/components/pages/ContactUs.js
@@ -1,25 +1,23 @@
-import React from 'react'
+import React from "react";
+import "./ContactUs.css";
 
-
-// https://www.youtube.com/watch?v=BtdM2_likL8 
+// https://www.youtube.com/watch?v=BtdM2_likL8
 // More complex version https://www.youtube.com/watch?v=KGFG-yQD7Dw
 
 function ContactUs() {
   return (
     <section>
-    <div className='AboutUs'>
-           <div className='container'>
-               <h1>Contact Us</h1>
+      <div className="contactUs">
+        <div className="contactUs-container">
+          <h1>Contact Us</h1>
 
-                   <div className='paragraph'>
-                   <p>insert info here</p>
-                   </div>
-
-           </div>
-       </div>
-   
-   </section>
-  )
+          <div className="contactUs-paragraph">
+            <p>insert info here</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default ContactUs
+export default ContactUs;
