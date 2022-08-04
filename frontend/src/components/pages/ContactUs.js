aa397261a87e365941670610768a533e70370153
Marga <margabovertorres@gmail.com>
2022-08-04 17:56:35 +0100

    WIP on the contact US form

--- frontend/src/components/pages/ContactUs.js
+++ frontend/src/components/pages/ContactUs.js
@@ -1,31 +1,66 @@
-import React from "react";
+import React, { createRef, useState } from "react";
 import "./ContactUs.css";
-import ContacUsInfo from "./ContactUsInfo";
 import ContactUsTitle from "./ContactUsTitle";
 import Card from "../ContactUs/Card";
+import { AiFillEnvironment } from "react-icons/ai";
 
 // https://www.youtube.com/watch?v=BtdM2_likL8
 // More complex version https://www.youtube.com/watch?v=KGFG-yQD7Dw
 
+function ContactUsInfo() {
+  return (
+    <Card>
+      <div className="">
+        <AiFillEnvironment />
+        <p>123 Road, London, L1 CGF4</p>
+      </div>
+    </Card>
+  );
+}
+
+function ContactUsForm() {
+  const emailInputRef = createRef();
+  const [isValid, setIsValid] = useState(true);
+
+  function validateEmail(email) {
+    return email.match(/^([a-z0-9_-]+\@[a-z0-9_-]+\.[a-z]+)$/);
+  }
+
+  function onBlurHandler() {
+    const email = String(emailInputRef.current.value).toLowerCase();
+    emailInputRef.current.value = email;
+    setIsValid(validateEmail(email));
+  }
+
+  return (
+    <Card>
+      <div className="contactUs-form">
+        <form>
+          <div>
+            <label for="email">email:</label>
+            <input
+              style={!isValid ? { color: "red" } : {}}
+              type="email"
+              name="email"
+              onBlur={onBlurHandler}
+              ref={emailInputRef}
+            />
+          </div>
+          <input type="text" />
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    </Card>
+  );
+}
+
 function ContactUs() {
   return (
     <div className="contactUs">
+      <ContactUsTitle heading="Contact Us" />
       <div className="container">
-        <div>
-          <div className="container">
-            <ContactUsTitle heading="Contact Us" />
-            <Card className="contactUs-card">
-              {/* <div className="right">contact form</div> */}
-              <div className="left">
-                <ContacUsInfo>Contact Info</ContacUsInfo>
-                {/* <ContactUsInfo1>Tlf</ContactUsInfo1> */}
-              </div>
-            </Card>
-            <Card>
-              <div className="contactUs-form">Contact Form</div>
-            </Card>
-          </div>
-        </div>
+        <ContactUsInfo />
+        <ContactUsForm />
       </div>
     </div>
   );
--- frontend/src/components/pages/ContactUsInfo.js
+++ frontend/src/components/pages/ContactUsInfo.js
@@ -1,32 +1,19 @@
 import React from "react";
-import styled from "styled-components";
-import { AiFillEnvironment } from "react-icons/ai";
+// import styled from "styled-components";
 
-const ItemStyle = styled.div`
-  padding: 2rem;
-  display: flex;
-  align-itmes: center;
-  gap: 2rem;
-  border: 8px;
-  margin-bottom: 2rem;
-  .icon {
-    color: var(--black);
-  }
-`;
+import Card from "../ContactUs/Card";
 
-export default function ContactUsInfo({
-  icon = <AiFillEnvironment />,
-  text = "123 Road, London, L1 CGF4",
-}) {
-  return (
-    <ItemStyle>
-      <div className="icon">{icon}</div>
-      <div className="info">
-        <p>{text}</p>
-      </div>
-    </ItemStyle>
-  );
-}
+// const ItemStyle = styled.div`
+//   padding: 2rem;
+//   display: flex;
+//   align-itmes: center;
+//   gap: 2rem;
+//   border: 8px;
+//   margin-bottom: 2rem;
+//   .icon {
+//     color: var(--black);
+//   }
+// `;
 
 // export default function ContactUsInfo1({
 //   icon = <AiFillEnvironment />,
