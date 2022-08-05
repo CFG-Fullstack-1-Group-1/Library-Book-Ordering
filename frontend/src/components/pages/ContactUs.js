0aea9108eae471bbdd2e15924f081f588d7081ed
Mariam <96213306+mazzilio@users.noreply.github.com>
2022-08-05 12:06:03 +0100

    Update ContactUs.js

--- frontend/src/components/pages/ContactUs.js
+++ frontend/src/components/pages/ContactUs.js
@@ -1,5 +1,5 @@
 import React from "react";
-import BookDetails from "../Test/Modal/BookDetails";
+
 
 // https://www.youtube.com/watch?v=BtdM2_likL8
 // More complex version https://www.youtube.com/watch?v=KGFG-yQD7Dw
@@ -11,9 +11,7 @@ function ContactUs() {
         <div className="container">
           <h1>Contact Us</h1>
 
-          <div>
-           <BookDetails/>
-          </div>
+      
         </div>
       </div>
     </section>
