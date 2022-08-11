d3234966290aff62dab5da163b09d412c1dadf74
mazzilio <mariam-hussein@outlook.com>
2022-08-11 14:11:30 +0100

    Styling Card

--- frontend/src/components/Form/OrderConfirmation.js
+++ frontend/src/components/Form/OrderConfirmation.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./OrderConfirmation.css";
 import { Route, Routes, useNavigate } from "react-router-dom";
 import Home from "../pages/Home";
+import ConfirmationIMG from "../../../static/images/confirmation-tick.png";
 
 function BookDetails({ title, authors, year, open, onClose }) {
   // Keeps it closed if not told to be open
@@ -13,24 +14,39 @@ function BookDetails({ title, authors, year, open, onClose }) {
   };
 
   return (
-    <div className="overlay">
-      <div className="overlay-inner">
-        <h1 className="book-title">Thank you for your order!</h1>
-        <div className="inner-box">
-          <h4>Your order details are as follows:</h4>
-          <div className="book-details">
-            Title: {title}, Authors: {authors}, Year: {year}
+    <div className="confirmation-container">
+      <div className="bookconf-overlay">
+        <div className="bookconf-overlay-inner">
+          <img
+            src={ConfirmationIMG}
+            alt="Confirmation tick"
+            className="confirmation-img"
+          />
+          <h1 className="confirmation-title">Order Confirmed</h1>
+          <div className="confirmation-inner-box">
+            <h4>
+              Your book request hs been successfully sent to be ordered you
+              should expect a response in 3 to 5 business days.
+            </h4>
+            <h3>Your order details are as follows:</h3>
+            <div className="book-details">
+              <p> Title: {title} </p>
+              <p>Authors: {authors}</p>
+              <p>Year: {year}</p>
+            </div>
+          </div>
+          <div className="confirmation-btn-container">
+            <button className="confirmation-btn" onClick={onClose}>
+              New Order
+            </button>
+            <button className="confirmation-btn" onClick={navigateToHome}>
+              OK
+            </button>
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
           </div>
         </div>
-        <button className="bookdetails-btn" onClick={onClose}>
-          New Order
-        </button>
-        <button className="bookdetails-btn" onClick={navigateToHome}>
-          OK
-        </button>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
       </div>
     </div>
   );
